5ba7e0e2c08f99f6a367f42741377919bcf31f0e
Jim Gagliardi <jvgagliardi98@outlook.com>
2020-12-29 17:08:59 -0500

    Added Basic Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _spd;
+    private CharacterController _controller;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3((horizontalInput * _spd), 0, 0);
+        _controller.Move(direction * Time.deltaTime);
+    }
+}
