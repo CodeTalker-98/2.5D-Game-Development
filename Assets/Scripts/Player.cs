7c71012a5d2e3fbb0e29df4c77b0dd4bd2861088
Jim Gagliardi <jvgagliardi98@outlook.com>
2020-12-30 15:28:15 -0500

    Added Jump and Double Jump Mechanics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _gravity = 9.81f;
+    [SerializeField] private float _jumpHeight = 15.0f;
     [SerializeField] private float _spd;
+    private bool _canDoubleJump = false;
+    private float _yVelocity;
     private CharacterController _controller;
 
     void Start()
@@ -26,12 +29,24 @@ public class Player : MonoBehaviour
 
         if (_controller.isGrounded)
         {
-            //Check if we can jump
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
         }
         else
         {
-            velocity.y -= _gravity;
+            if (Input.GetKeyDown(KeyCode.Space) && _canDoubleJump)
+            {
+                _yVelocity += _jumpHeight;
+                _canDoubleJump = false;
+            }
+
+            _yVelocity -= _gravity;
         }
+
+        velocity.y = _yVelocity;
 
         _controller.Move(velocity * Time.deltaTime);
     }
