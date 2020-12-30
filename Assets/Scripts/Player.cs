1c2246da5b326172bb832015cbeaaae18d3a051e
Jim Gagliardi <jvgagliardi98@outlook.com>
2020-12-30 10:37:07 -0500

    Added Gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _gravity = 9.81f;
     [SerializeField] private float _spd;
     private CharacterController _controller;
 
@@ -22,6 +23,16 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector3 direction = new Vector3(horizontalInput, 0, 0);
         Vector3 velocity = direction * _spd;
+
+        if (_controller.isGrounded)
+        {
+            //Check if we can jump
+        }
+        else
+        {
+            velocity.y -= _gravity;
+        }
+
         _controller.Move(velocity * Time.deltaTime);
     }
 }
